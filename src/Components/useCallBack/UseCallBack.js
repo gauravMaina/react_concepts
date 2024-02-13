25847d8f655af8c86c2715230faa2a43271e8ed5
gauravMaina <gauravjadhav839@gmail.com>
2024-02-13 11:40:25 +0530

    use callback

--- src/Components/useCallBack/UseCallBack.js
+++ src/Components/useCallBack/UseCallBack.js
@@ -2,23 +2,22 @@ import React, { useCallback, useMemo, useState } from 'react'
 import Parent from './Parent'
 
 function UseCallBack() {
-    const [counterone, setCounterone] = useState(0)
+  //all states
+  const [counterone, setCounterone] = useState(0)
+  const [counterTwo, setCounterTwo] = useState(0)
 
-    const isEven = useMemo(()=>{
-        return counterone%2===0
-    },[counterone])
-
-
-    const incrementOne =useCallback(()=>{setCounterone(counterone + 1)},[counterone])
+  const incrementOne = () => { setCounterone(counterone + 1) }
+  const Learning =useCallback(()=>{},[counterTwo])
   return (
     <div>
-      <Parent counterone={counterone} incrementOne={incrementOne}/>
-
+      <Parent counterTwo={counterTwo} setCounterTwo={setCounterTwo} Learning={Learning} />
       <div className='use-memo-counter'>
-                <button className='btn' onClick={incrementOne}>counterOne</button>
-                <span className='counter-value'>{counterone}</span>
-                <span className='counter-value'>{isEven?'Even':'Odd'}</span>
-            </div>
+        <button className='btn' onClick={incrementOne}>counterOne</button>
+        <span className='counter-value'>{counterone}</span>
+      </div>
+      <h2>counter two</h2>
+      <button className='btn' onClick={()=>setCounterTwo(counterTwo+1)}>counterTwo</button>
+        <span className='counter-value'>{counterTwo}</span>
     </div>
   )
 }
